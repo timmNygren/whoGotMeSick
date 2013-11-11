9038e33db84dd7063a06da2f21160372ec52e605
Adam Bodnar <abodnar@mines.edu>
2013-11-10 22:08:37 -0700

    basic html and stylesheet

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Who Got Me Sick</title>
+<link rel="stylesheet" type="text/css" href="whogotmesick.css">
+</head>
+<body>
+	<article>
+	  <h1>Test 1</h1>
+	  <p>Test Text</p>
+	</article>
+
+	<article>
+	  <h1>Test 2</h1>
+	  <p>Test Text</p>
+	</article>
+
+	<article>
+	  <h1>Test 3</h1>
+	  <p>Test Text</p>
+	</article>
+</body>
+</html>
