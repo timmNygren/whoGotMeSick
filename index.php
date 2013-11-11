3124fe83394e38025f5adf862109644df7486e15
Adam Bodnar <abodnar@mines.edu>
2013-11-11 00:10:11 -0700

    more styling and worthless div button

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 <html>
 <head>
 	<title>Who Got Me Sick</title>
-	<article class="top">
-		<h1>whogotmesick.com</h1>
-	</article>
-<link rel="stylesheet" type="text/css" href="whogotmesick.css">
+	<link rel="stylesheet" type="text/css" href="whogotmesick.css">
 </head>
 <body>
+	<article class="top">
+		<h1>whogotmesick.com</h1>
+			<div class="login">
+		<p>Login</p>
+	</div>
+	</article>
 	<article class="main">
 	  <h1>User</h1>
 	  <p>Test Text</p>
@@ -28,5 +31,6 @@
 	  <p class="top_right">11/10/2012</p>
 	  <p class="bottom_right">9001 points</p>
 	</article>
+	
 </body>
 </html>
