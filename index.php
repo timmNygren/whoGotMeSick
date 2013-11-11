75ee62012c55216a54becc2b6139e0de3ae81c2b
Adam Bodnar <abodnar@mines.edu>
2013-11-10 22:35:11 -0700

    more styling and html

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Who Got Me Sick</title>
+	<title>Who Got Me Sick</title>
+	<article class="top">
+	  <h1>whogotmesick.com</h1>
+	</article>
 <link rel="stylesheet" type="text/css" href="whogotmesick.css">
 </head>
 <body>
-	<article>
-	  <h1>Test 1</h1>
+	<article class="main">
+	  <h1>User</h1>
 	  <p>Test Text</p>
+	  <p class="top_right">Date</p>
+	  <p class="bottom_right">Score</p>
 	</article>
 
-	<article>
-	  <h1>Test 2</h1>
+	<article class="main">
+	  <h1>User</h1>
 	  <p>Test Text</p>
+	  <p class="top_right">Date</p>
+	  <p class="bottom_right">Score</p>
 	</article>
 
-	<article>
-	  <h1>Test 3</h1>
+	<article class="main">
+	  <h1>User</h1>
 	  <p>Test Text</p>
+	  <p class="top_right">Date</p>
+	  <p class="bottom_right">Score</p>
 	</article>
 </body>
 </html>
