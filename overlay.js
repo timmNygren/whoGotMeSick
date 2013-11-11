81e5321130aab8a411f0e4b2a62c8f454a31d6d8
Hunter Lang <hunter@orbotix.com>
2013-11-11 00:17:13 -0700

    Overlay is now working as intended.

--- overlay.js
+++ overlay.js
@@ -2,7 +2,7 @@ function onClick() {
 	var divElement = document.getElementById("displaybox");
 	if (divElement.style.display == "none") {
 		divElement.style.display = "";
-		divElement.innerHTML = "<table height='100%' width='25%'><tr><td><strong>SUCK MAH DICK</strong></td></tr></table>";	
+		divElement.innerHTML = "<table height='100%' width='100%'><tr><td><a href='#' onClick='return onClick()'><strong>SUCK MAH DICK</strong></a></td></tr></table>";	
 	}
 	else {
 		divElement.style.display = "none";
