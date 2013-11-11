3d2bef1ec1554f6e4f87a90b8b765e3a8848e1e1
Adam Bodnar <abodnar@mines.edu>
2013-11-10 23:14:08 -0700

    more styling and html

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Who Got Me Sick</title>
 	<article class="top">
-	  <h1>whogotmesick.com</h1>
+		<h1>whogotmesick.com</h1>
 	</article>
 <link rel="stylesheet" type="text/css" href="whogotmesick.css">
 </head>
@@ -11,22 +11,22 @@
 	<article class="main">
 	  <h1>User</h1>
 	  <p>Test Text</p>
-	  <p class="top_right">Date</p>
-	  <p class="bottom_right">Score</p>
+	  <p class="top_right">11/10/2012</p>
+	  <p class="bottom_right">9001 points</p>
 	</article>
 
 	<article class="main">
 	  <h1>User</h1>
 	  <p>Test Text</p>
-	  <p class="top_right">Date</p>
-	  <p class="bottom_right">Score</p>
+	  <p class="top_right">11/10/2012</p>
+	  <p class="bottom_right">9001 points</p>
 	</article>
 
 	<article class="main">
 	  <h1>User</h1>
 	  <p>Test Text</p>
-	  <p class="top_right">Date</p>
-	  <p class="bottom_right">Score</p>
+	  <p class="top_right">11/10/2012</p>
+	  <p class="bottom_right">9001 points</p>
 	</article>
 </body>
 </html>
