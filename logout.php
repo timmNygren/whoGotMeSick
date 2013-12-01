fedfb96300d959713c01ff3bd49f53d526febd3f
Hunter Lang <hunter@orbotix.com>
2013-12-01 14:33:01 -0700

    Logout button works now.

--- logout.php
+++ logout.php
@@ -1,25 +1,8 @@
 <?php
 
 	session_start();
-
-	$redirect = NULL;
-	if (!empty($_POST)) {
-
-		if (isset($_GET['location'])) {
-			$redirect = $_GET['location'];
-		}
-
-		if (isset($_POST['logout'])) {
-
-			unset($_SESSION['valid_user']);
-			session_destroy();
-
-			header("Location:".$redirect);
-			exit();
-		} elseif (isset($_SESSION['valid_user'])) {
-			header("Location:".$redirect);
-			exit();
-		}
-	}
-
+	unset($_SESSION['valid_user']);
+	session_destroy();
+	header("Location:index.php");
+	exit();
 ?>
