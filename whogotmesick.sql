9920c9bc9a457360f2e9a35ace70b2b34e36f791
Hunter Lang <hunter@orbotix.com>
2013-12-03 01:43:48 -0700

    Removed insert statements.

--- whogotmesick.sql
+++ whogotmesick.sql
@@ -19,18 +19,3 @@ create table reports (
 		symptoms char(9) not null,
 		note text,
 		report_date DATE not null);
-
-insert into users(username, password, settings, date_joined)
-	values("tnygren", "drowssap", "00", NOW()),
-			("hlang", "password", "00", NOW()),
-			("abodnar", "superpassword", "00", NOW()),
-			("testUser", "testPassword", "00", NOW());
-
-insert into reports(user_id, zip_code, symptoms, note, report_date)
-	values(1, 81637, "000000000", NULL, CURDATE()),
-		  (2, 80401, "111111111", "This cold kicked my butt!", CURDATE()),
-		  (3, 92014, "000000000", "Not too bad of a cold", CURDATE()),
-		  (4, 81631, "000000001", "Super bad cold, I had no symptoms", CURDATE()),
-		  (1, 80401, "100101011", "Somewhat bad cold", "2013-11-10"),
-		  (2, 81631, "110111111", "Pretty bad", "2013-10-10"),
-		  (3, 80401, "000010101", "Text", "2013-11-25");
