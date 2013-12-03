d9787f8fead4b6236f146c31a7c3e42484693713
Hunter Lang <hunter@orbotix.com>
2013-12-03 00:37:13 -0700

    Fix issue where title bar doubles itself.

--- index.php
+++ index.php
@@ -61,32 +61,6 @@
 		}
 		include('indexpagetitlebar.php');
 	?>
-	<article class="top">
-		<div id="logo"></div>
-	<?php
-		if (isset($_SESSION['valid_user'])) {
-		
-			echo "<div id='overview_button' onClick='onOverviewClicked();'></div>";
-			// Button for the account page link
-			
-			echo "<div id='account_button' onClick='onAccountClicked();'></div>";
-
-			// Button for the report page link
-			echo "<div id='report_button'></div>";
-
-			// Button for logout
-			echo "<div id='logout_button' onClick='onLogoutClicked();'></div>";
-		} else {
-
-			if (isset($username)) {
-				// Tried to log in and failed
-				echo "Could not log you in";
-			}
-
-			echo "<div id='login_button'></div>";
-		}
-
-	?>
 	</article>
 	<div class="search_area">
 		<form action="index.php" method="get">
